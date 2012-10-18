1f10d301e1d1ff4993291fd4c1d8efdebfc49453
unknown <hpinto@.bar.lx>
2012-10-18 17:52:50 +0100

    finished hw2 part01

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,3 @@
 module ApplicationHelper
-  def sortable(column,title=nil)
-    title||=column.titleize
-    css_class = column == params[:sort] ? "hilite" : nil
-    link_to title, {:sort=>column},{:class=>css_class}
-  end
+
 end
