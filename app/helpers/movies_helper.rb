042b5706800288dc04e8c7797990af0886b4c899
unknown <hpinto@.bar.lx>
2012-10-18 17:52:15 +0100

    finished part 01

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -3,5 +3,12 @@ module MoviesHelper
   def oddness(count)
     count.odd? ?  "odd" :  "even"
   end
-
+  def sortable(column,title=nil)
+    title||=column.titleize
+    css_class = column == params[:sort] ? "hilite" : nil
+    link_to title, {:sort=>column},{:class=>css_class}
+  end
+  def set_th_class(column)
+    column == params[:sort] ? "hilite" : nil
+  end
 end
